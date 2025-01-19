cc589537d48bdf7595172f2eefb9326f5157b4e0
GabrielMornard <Gabrielmornard@gmail.com>
2025-01-19 15:04:08 -0500

    PROGRESS of the movie details

--- starwars_wiki/starwars_wiki/src/app/components/movieDetails/movieDetails.component.ts
+++ starwars_wiki/starwars_wiki/src/app/components/movieDetails/movieDetails.component.ts
@@ -10,6 +10,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MovieDetailsComponent implements OnInit {
 
   @Input() movie!: Movie;
+  @Input() imgUrl!: String;
   constructor() { }
 
   ngOnInit() {
